9da99f486b8e618a79e9109f93a32194e35932af
Michael Teixeira <meixeira@gmail.com>
2018-06-25 17:25:40 +0200

    Add Migration Script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,11 +1,29 @@
-const FelixPool = artifacts.require('./FelixPool');
-const BigNumber = web3.BigNumber;
+const PoolbaseFactory = artifacts.require('./PoolbaseFactory');
+const PoolbaseEventEmitter = artifacts.require('./PoolbaseEventEmitter');
 
-const threshold = new BigNumber(100);
-const cap = new BigNumber(200);
-const endTime = web3.eth.getBlock('latest').timestamp + 86400 * 20; // 20 days
-const rate = new BigNumber(10);
 
-module.exports = function(deployer, network, [admin, wallet]) {
-    deployer.deploy(FelixPool, threshold, cap, endTime, rate, wallet);
+module.exports = async function(deployer, network, [coinbaseAccount, poolAdmin]) {
+  await deployer.deploy(PoolbaseFactory);
+  await deployer.deploy(PoolbaseEventEmitter);
+
+  const poolParams = {
+   superBouncers: [coinbaseAccount],
+   maxAllocation: 200e18,
+   adminPoolFee: [5, 1000],
+   poolbaseFee: [5, 1000],
+   isAdminFeeInWei: true,
+   payoutWallet: coinbaseAccount,
+   adminPayoutWallet: coinbaseAccount,
+   poolbasePayoutWallet: coinbaseAccount,
+   eventEmitterContract: PoolbaseEventEmitter.address,
+   admins: [poolAdmin]
+  };
+
+  const factoryInstance = PoolbaseFactory.at(PoolbaseFactory.address);
+  const params = [...Object.values(poolParams)];
+  console.log('params', params);
+  const result = await factoryInstance.create(...params, {from: coinbaseAccount});
+
+  console.log(result);
+
 };
