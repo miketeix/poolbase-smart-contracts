3a30bed5e5087431aea29622d3d3dad2e85190d2
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-06-28 18:49:50 +0200

    newer version of signature bouncer

---
+++ contracts/helpers/SignatureBouncerMock.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.4.23;
+
+import "./SignatureBouncer2.sol";
+
+
+contract SignatureBouncerMock is SignatureBouncer {
+    function checkValidSignature(address _address, bytes _sig)
+        public
+        view
+        returns (bool)
+    {
+        return isValidSignature(_address, _sig);
+    }
+
+    function onlyWithValidSignature(bytes _sig) public onlyValidSignature(_sig) view {}
+
+    function checkValidSignatureAndMethod(address _address, bytes _sig)
+        public
+        view
+        returns (bool)
+    {
+        return isValidSignatureAndMethod(_address, _sig);
+    }
+
+    function onlyWithValidSignatureAndMethod(bytes _sig) public onlyValidSignatureAndMethod(_sig) view
+    {}
+
+    function checkValidSignatureAndData(address _address, bytes _bytes, uint _val, bytes _sig)
+        public
+        view
+        returns (bool)
+    {
+        return isValidSignatureAndData(_address, _sig);
+    }
+
+    function onlyWithValidSignatureAndData(uint _val, bytes _sig) public onlyValidSignatureAndData(_sig) view
+    {}
+}
