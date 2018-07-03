72b0544785bcb2d588886a85b26097f005ecd96c
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-07-04 00:21:01 +0200

    fix migration file

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -7,33 +7,39 @@ module.exports = async function(
     network,
     [coinbaseAccount, poolAdmin]
 ) {
-  await deployer.deploy(PoolbaseFactory);
-  await deployer.deploy(PoolbaseEventEmitter);
+    await deployer.deploy(PoolbaseFactory);
+    await deployer.deploy(PoolbaseEventEmitter);
 
-  const poolParams = {
-      superBouncers: [coinbaseAccount],
-      maxAllocation: 200e18,
-      adminPoolFee: [5, 1000],
-      poolbaseFee: [5, 1000],
-      isAdminFeeInWei: true,
-      payoutWallet: coinbaseAccount,
-      adminPayoutWallet: coinbaseAccount,
-      poolbasePayoutWallet: coinbaseAccount,
-      eventEmitterContract: PoolbaseEventEmitter.address,
-      admins: [poolAdmin]
-  };
+    const poolParams = {
+        superBouncers: [coinbaseAccount],
+        maxAllocation: 200e18,
+        adminPoolFee: [5, 1000],
+        poolbaseFee: [5, 1000],
+        isAdminFeeInWei: true,
+        payoutWallet: coinbaseAccount,
+        adminPayoutWallet: coinbaseAccount,
+        poolbasePayoutWallet: coinbaseAccount,
+        eventEmitterContract: PoolbaseEventEmitter.address,
+        admins: [poolAdmin]
+    };
 
-  const factoryInstance = PoolbaseFactory.at(PoolbaseFactory.address);
-  const params = [...Object.values(poolParams)];
+    const factoryInstance = PoolbaseFactory.at(PoolbaseFactory.address);
+    const params = [...Object.values(poolParams)];
 
-  const receipt = await factoryInstance.create(...params, {
-      from: coinbaseAccount
-  });
-  const poolAddress = receipt.logs[0].address;
-  console.log('poolAddress', poolAddress);
-  
-  // const isAdminFeeInWei = await poolInstance.isAdminFeeInWei();
-  // console.log('isAdminFeeInWei', isAdminFeeInWei);
+    await factoryInstance.create(...params, {
+        from: coinbaseAccount
+    });
+    const poolAddress = await factoryInstance.instantiations.call(
+        coinbaseAccount,
+        0
+    );
+    console.log('poolAddress', poolAddress);
 
-  // console.log('poolInstance.maxAllocation', poolInstance.maxAllocation);
+    const poolInstance = PoolContract.at(poolAddress);
+
+    const isAdminFeeInWei = await poolInstance.isAdminFeeInWei();
+    console.log('isAdminFeeInWei', isAdminFeeInWei);
+
+    const maxAllocation = await poolInstance.maxAllocation();
+    console.log('poolInstance.maxAllocation', maxAllocation);
 };
