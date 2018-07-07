867eabe18b79833f911f9d016493dad4d5a19281
Michael Teixeira <meixeira@gmail.com>
2018-07-07 12:04:10 -0500

    Adding truffle scripts to aid dev testing * exec scripts are run using the following command:  truffle exec ./test/scripts/createPool.js * on each truffle deploy, poolbaseFactory and EventEmitter addresses are stored in file to be used by exec scripts * added web3 1.0 for use in exec scripts

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var path = require('path');
+
 const PoolbaseFactory = artifacts.require('./PoolbaseFactory');
 const PoolbaseEventEmitter = artifacts.require('./PoolbaseEventEmitter');
 const PoolContract = artifacts.require('./Poolbase');
+const addressesPath = path.join(__dirname, '../addresses');
 
 module.exports = async function(
     deployer,
@@ -10,6 +14,9 @@ module.exports = async function(
     await deployer.deploy(PoolbaseFactory);
     await deployer.deploy(PoolbaseEventEmitter);
 
+    writeAddressFile(PoolbaseFactory, 'poolbaseFactory');
+    writeAddressFile(PoolbaseEventEmitter, 'poolbaseEventEmitter');
+
     const poolParams = {
         superBouncers: [coinbaseAccount],
         maxAllocation: 200e18,
@@ -17,7 +24,7 @@ module.exports = async function(
         poolbaseFee: [5, 1000],
         isAdminFeeInWei: true,
         payoutWallet: coinbaseAccount,
-        adminPayoutWallet: coinbaseAccount,
+        adminPayoutWallet: poolAdmin,
         poolbasePayoutWallet: coinbaseAccount,
         eventEmitterContract: PoolbaseEventEmitter.address,
         admins: [poolAdmin]
@@ -34,13 +41,17 @@ module.exports = async function(
         coinbaseAccount,
         0
     );
-    console.log('poolAddress', poolAddress);
 
     const poolInstance = PoolContract.at(poolAddress);
 
-    const isAdminFeeInWei = await poolInstance.isAdminFeeInWei();
-    console.log('isAdminFeeInWei', isAdminFeeInWei);
-
-    const maxAllocation = await poolInstance.maxAllocation();
-    console.log('poolInstance.maxAllocation', maxAllocation);
 };
+
+const writeAddressFile = (contract, filename) => {
+
+  fs.writeFile(`${addressesPath}/${filename}`, contract.address, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The filename file was saved!");
+  });
+}
