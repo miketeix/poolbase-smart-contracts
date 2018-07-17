b26b1fd1d2ac6b604339ef3299700e40032f82d0
Michael Teixeira <meixeira@gmail.com>
2018-07-17 16:37:15 -0500

    Un-comment migration script - works well!

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -22,30 +22,30 @@ module.exports = async function(
         from: coinbaseAccount
     });
 
-    // await factoryInstance.setPoolbasePayoutWallet(coinbaseAccount, { from: coinbaseAccount });
-    // await factoryInstance.setPoolbaseFee([5, 1000], { from: coinbaseAccount });
+    await factoryInstance.setPoolbasePayoutWallet(coinbaseAccount, { from: coinbaseAccount });
+    await factoryInstance.setPoolbaseFee([5, 1000], { from: coinbaseAccount });
 
-    // const poolParams = {
-    //     maxAllocation: 200e18,
-    //     adminPoolFee: [5, 1000],
-    //     isAdminFeeInWei: true,
-    //     payoutWallet: coinbaseAccount,
-    //     adminPayoutWallet: poolAdmin,
-    //     eventEmitterContract: PoolbaseEventEmitter.address,
-    //     admins: [poolAdmin]
-    // };
-    // const params = [...Object.values(poolParams)];
-    //
-    // await factoryInstance.create(...params, {
-    //     from: coinbaseAccount
-    // });
-    //
-    // const poolAddress = await factoryInstance.instantiations.call(
-    //     coinbaseAccount,
-    //     0
-    // );
-    //
-    // const poolInstance = PoolContract.at(poolAddress);
+    const poolParams = {
+        maxAllocation: 200e18,
+        adminPoolFee: [5, 1000],
+        isAdminFeeInWei: true,
+        payoutWallet: coinbaseAccount,
+        adminPayoutWallet: poolAdmin,
+        eventEmitterContract: PoolbaseEventEmitter.address,
+        admins: [poolAdmin]
+    };
+    const params = [...Object.values(poolParams)];
+
+    await factoryInstance.create(...params, {
+        from: coinbaseAccount
+    });
+
+    const poolAddress = await factoryInstance.instantiations.call(
+        coinbaseAccount,
+        0
+    );
+
+    const poolInstance = PoolContract.at(poolAddress);
 };
 
 const writeAddressFile = (contract, filename) => {
