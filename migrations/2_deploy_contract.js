f9b984e33544763f214e2e5329896486ef0caf1d
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-08-20 20:30:31 +0200

    migration reference refactored contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,7 +3,7 @@ var path = require("path");
 
 const PoolbaseFactory = artifacts.require("./PoolbaseCloneFactory");
 const PoolbaseEventEmitter = artifacts.require("./PoolbaseEventEmitter");
-const PoolContract = artifacts.require("./PoolbaseV2");
+const PoolContract = artifacts.require("./Poolbase");
 const addressesPath = path.join(__dirname, "../addresses");
 
 module.exports = async function(
@@ -14,23 +14,17 @@ module.exports = async function(
   await deployer.deploy(PoolContract);
   await deployer.deploy(PoolbaseFactory, PoolContract.address);
   await deployer.deploy(PoolbaseEventEmitter);
-
   writeAddressFile(PoolContract, "poolbaseCloneLibrary");
   writeAddressFile(PoolbaseFactory, "poolbaseFactory");
   writeAddressFile(PoolbaseEventEmitter, "poolbaseEventEmitter");
-
   const factoryInstance = await PoolbaseFactory.at(PoolbaseFactory.address);
-
   await factoryInstance.setSuperBouncers([coinbaseAccount, superBouncer], {
     from: coinbaseAccount
   });
-
   await factoryInstance.setPoolbasePayoutWallet(coinbaseAccount, {
     from: coinbaseAccount
   });
-
   await factoryInstance.setPoolbaseFee([5, 1000], { from: coinbaseAccount });
-
   const poolParams = {
     maxAllocation: 200e18,
     adminPoolFee: [5, 1000],
@@ -41,24 +35,19 @@ module.exports = async function(
     admins: [poolAdmin]
   };
   const params = [...Object.values(poolParams)];
-
   await factoryInstance.create(...params, {
     from: coinbaseAccount
   });
-
   const poolAddress = await factoryInstance.instantiations.call(
     coinbaseAccount,
     0
   );
-
   const poolInstance = PoolContract.at(poolAddress);
-
   const isSuperBouncer = await poolInstance.hasRole(superBouncer, "bouncer");
   const isCoinbaseAccountSuperBouncer = await poolInstance.hasRole(
     coinbaseAccount,
     "bouncer"
   );
-
   console.log({ isSuperBouncer, isCoinbaseAccountSuperBouncer });
 };
 
